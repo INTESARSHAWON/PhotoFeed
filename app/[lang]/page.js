63e4250a4066f40ac225510cdbb514d7068409e0
Intesar Shawon <intesarshawon@gmail.com>
2025-07-05 04:08:39 +0600

    decorate ui with a photo from learn with sumit

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,8 +1,17 @@
+import PhotoList from "@/components/PhotoList";
 import { getDictionary } from "./dictionaries";
 
-export default async function Home({params: {lang}}) {
-  const dictionary = await getDictionary(lang);
+// dictionary(bangla to english) purposes
+// export default async function Home({params: {lang}}) {
+//   const dictionary = await getDictionary(lang);
+export default async function Home() {
+  console.log(process.env.BASE_API_URL);
+
+  const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+  const photos = await response.json();
+  
   return (
-    <div>{dictionary.followers}</div>
+    // <div>{dictionary.followers}</div>
+    <PhotoList photos={photos} />
   );
 }
